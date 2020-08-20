2af5b6a664a70c2bb514ab07b0562b74388a3449
Baumel, Dvir (db384r) <db384r@intl.att.com>
2020-08-20 19:30:52 +0300

    configurations

--- Encryptors_Project/src/main/java/Main.java
+++ Encryptors_Project/src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(MainConfiguration.class);
         RegisterManager registerManager = ctx.getBean("registerManager", RegisterManager.class);
+        RegisterManager registerManager = ctx.getBean("registerManager", RegisterManager.class);
 
         try {
             registerManager.run();
